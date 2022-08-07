3a59c108ad65e0a1979b2edcc0401da22e7a677b
Seioo Inoue <sei0o@live.jp>
2022-08-07 16:15:30 +0900

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# binchotan
+
+## Usage
+
+1. Clone this repository
+2. Set the environment variable `TWITTER_API_KEY` and `TWITTER_API_SECRET` to your OAuth2 (not 1.0a) credentials, or use `.env`.
+3. `cargo run`
+4. Use the mock frontend to see it works.
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+
+    // TODO: authenticate to Twitter
+
+    // TODO: listen to request over sockets...
+    // TODO: obtain timeline tweets from twitter API
+    // TODO: call filters in Lua
+    // TODO: return filtered tweets over the socket
+    // TODO: mock frontend
+    // TODO: protocol?
+}
