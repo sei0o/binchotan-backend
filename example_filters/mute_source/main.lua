986ddc47e8c9376f35b326e94a959988f33a9963
Nakaya <eniehack@outlook.jp>
2022-08-26 19:41:32 +0900

    add target sources

--- example_filters/mute_source/main.lua
+++ example_filters/mute_source/main.lua
@@ -1,4 +1,10 @@
-if post.source:find "Peing" or post.source:find "ツイ廃あらーと" then
+if post.source:find "Peing" or
+   post.source:find "ツイ廃あらーと" or
+   post.source:find "今日のツイライフ" or
+   post.source:find "ツイ廃ジャー" or
+   post.source:find "contributter" or
+   post.source:find "twttbot.net" or
+   post.source:find "Githubiter" then
   return nil
 else
   return post
