c7681b90f240492ca41dd21e40ae9fe39dd7b46c
sei0o <sei0o@live.jp>
2022-09-08 20:41:27 +0900

    mute 相談箱 source

--- example_filters/mute_source/main.lua
+++ example_filters/mute_source/main.lua
@@ -2,6 +2,7 @@ if post.source:find "Peing" or
    post.source:find "ツイ廃あらーと" or
    post.source:find "今日のツイライフ" or
    post.source:find "ツイ廃ジャー" or
+   post.source:find "相談箱" or
    post.source:find "contributter" or
    post.source:find "twttbot.net" or
    post.source:find "Githubiter" then
