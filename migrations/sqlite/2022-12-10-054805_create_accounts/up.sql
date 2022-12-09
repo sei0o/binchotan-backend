ec11d13906197afc5fb9497f6479c562702305ef
Nakaya <eniehack@outlook.jp>
2022-12-10 05:54:27 +0900

    add migration script for sqlite

---
+++ migrations/sqlite/2022-12-10-054805_create_accounts/up.sql
@@ -0,0 +1,8 @@
+create table accounts (
+  id integer primary key autoincrement,
+  twitter_id text unique not null,
+  access_token text unique not null,
+  refresh_token text unique not null,
+  session_key text unique,
+  owned_by integer references accounts (id)
+);
